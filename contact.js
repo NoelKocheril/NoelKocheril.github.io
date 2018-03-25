5f75c55df2222d4798ad0473d4f235466b49c6e2
NoelKocheril <NoelKocheril@hotmail.com>
2018-03-24 20:05:51 -0400

    FormSpree Test

--- contact.js
+++ contact.js
@@ -4,5 +4,7 @@
 $('#contact-form').on('submit', function(e) {
   $('#contact-form *').fadeOut(2000);
   $('#contact-form').prepend('Your submission has been processed...');
+  var form = document.getElementById("contact-form");
+	form.reset();
   });
 </script>
